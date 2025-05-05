5167dd9e80f078c79ecef06e39bb68b7c29f8f2c
Gabriel Diaz <dg22023@ues.edu.sv>
2025-05-05 17:32:42 -0600

    Restructuracion Dashboard-students y creacion main menu en PAGES

---
+++ frontproyectodaw/src/app/pages/dashboard-students/dashboard-students.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DashboardStudentsComponent } from './dashboard-students.component';
+
+describe('DashboardStudentsComponent', () => {
+  let component: DashboardStudentsComponent;
+  let fixture: ComponentFixture<DashboardStudentsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DashboardStudentsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DashboardStudentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontproyectodaw/src/app/pages/main-menu/main-menu.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main-menu',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.scss'
+})
+export class MainMenuComponent {
+  title = '';
+
+  constructor(private router: Router) {}
+
+  //functionto redirect
+  redirectTo(path: string) {
+    this.router.navigate([path]);
+  }
+}
